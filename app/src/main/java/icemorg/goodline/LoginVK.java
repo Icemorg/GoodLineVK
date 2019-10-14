0cf71d0d8d20c44749941753ef205641eac83dcb
Icemorg <icemorg@gmail.com>
2019-10-14 18:11:39 +0700

    users

--- app/src/main/java/icemorg/goodline/LoginVK.java
+++ app/src/main/java/icemorg/goodline/LoginVK.java
@@ -2,20 +2,29 @@ package icemorg.goodline;
 
 import android.app.Application;
 import android.content.Intent;
-
-import com.vk.api.sdk.VK;
-import com.vk.api.sdk.VKTokenExpiredHandler;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKAccessTokenTracker;
+import com.vk.sdk.VKSdk;
 
 public class LoginVK extends Application {
 
-    VKTokenExpiredHandler vkTokenExpiredHandler = new VKTokenExpiredHandler() {
+//    VKTokenExpiredHandler vkTokenExpiredHandler = new VKTokenExpiredHandler() {
+//        @Override
+//        public void onTokenExpired() {
+//                Intent intent = new Intent(LoginVK.this, MainActivity.class);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
+//        }
+//    };
+
+    VKAccessTokenTracker vkAccessTokenTracker = new VKAccessTokenTracker() {
         @Override
-        public void onTokenExpired() {
-//            if (newToken == null) {
+        public void onVKAccessTokenChanged(VKAccessToken oldToken, VKAccessToken newToken) {
+            if (newToken == null) {
                 Intent intent = new Intent(LoginVK.this, MainActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
-//            }
+            }
         }
     };
 
@@ -23,8 +32,10 @@ public class LoginVK extends Application {
     public void onCreate() {
         super.onCreate();
 
-        VK.addTokenExpiredHandler(vkTokenExpiredHandler);
-        VK.initialize(this);
+//        VK.addTokenExpiredHandler(vkTokenExpiredHandler);
+//        VK.initialize(this);
+        vkAccessTokenTracker.startTracking();
+        VKSdk.initialize(this);
     }
 
 }
--- app/src/main/java/icemorg/goodline/MainActivity.java
+++ app/src/main/java/icemorg/goodline/MainActivity.java
@@ -1,49 +1,90 @@
 package icemorg.goodline;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
-import com.vk.api.sdk.VK;
-import com.vk.api.sdk.auth.VKAccessToken;
-import com.vk.api.sdk.auth.VKAuthCallback;
-import com.vk.api.sdk.auth.VKScope;
-import com.vk.api.sdk.utils.VKUtils;
-
-import org.jetbrains.annotations.NotNull;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKList;
+import com.vk.sdk.util.VKUtil;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private VKScope[] scope = new VKScope[]{VKScope.MESSAGES, VKScope.FRIENDS, VKScope.WALL};
+//    private VKScope[] scope = new VKScope[]{VKScope.FRIENDS, VKScope.PHOTOS, VKScope.WALL}; //211
+    private String[] scope = new String[]{VKScope.FRIENDS, VKScope.PHOTOS, VKScope.WALL};
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        VK.initialize(this);
-        String[] fingerprints = VKUtils.getCertificateFingerprint(this, this.getPackageName());
+
+        String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
         System.out.println(Arrays.asList(fingerprints));
-        VK.login(this, Arrays.asList(scope));
+//        VK.login(this, Arrays.asList(scope));
+        VKSdk.login(this, scope);
     }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (!VK.onActivityResult(requestCode, resultCode, data, new VKAuthCallback() {
+//
+//            @Override
+//            public void onLoginFailed(int i) {
+//                Toast.makeText(getApplicationContext(),"NO ACCESS!", Toast.LENGTH_LONG).show();
+//            }
+//
+//            @Override
+//            public void onLogin(@NotNull VKAccessToken vkAccessToken) {
+//                recyclerView = findViewById(R.id.recyclerView);
+//                Toast.makeText(getApplicationContext(),"Good!", Toast.LENGTH_LONG).show();
+//            }
+//
+//        })) {
+//            super.onActivityResult(requestCode, resultCode, data);
+//        }
+//    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (!VK.onActivityResult(requestCode, resultCode, data, new VKAuthCallback() {
-
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
             @Override
-            public void onLoginFailed(int i) {
-                Toast.makeText(getApplicationContext(),"NO ACCESS!", Toast.LENGTH_LONG).show();
-            }
+            public void onResult(VKAccessToken res) {
+                recyclerView = findViewById(R.id.recyclerView);
 
-            @Override
-            public void onLogin(@NotNull VKAccessToken vkAccessToken) {
+                VKRequest request = VKApi.users().get(VKParameters.from(VKApiConst.FIELDS, "first_name", "last_name"));
+                request.executeWithListener(new VKRequest.VKRequestListener() {
+                    @Override
+                    public void onComplete(VKResponse response) {
+                        super.onComplete(response);
+
+                        VKList list = (VKList) response.parsedModel;
+                        ArrayAdapter<String> arrayAdapter =new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_expandable_list_item_1, list);
+
+//                        recyclerView.setAdapter(arrayAdapter);
+                    }
+                });
                 Toast.makeText(getApplicationContext(),"Good!", Toast.LENGTH_LONG).show();
             }
-
+            @Override
+            public void onError(VKError error) {
+                Toast.makeText(getApplicationContext(),"Error!", Toast.LENGTH_LONG).show();
+            }
         })) {
             super.onActivityResult(requestCode, resultCode, data);
         }
